2a6462e2d669f20f02efa1f13d4355af86a40623
rog-works <rog_works@yahoo.co.jp>
2016-08-11 06:58:46 +0000

    fix

--- app/public/js/models/md.js
+++ app/public/js/models/md.js
@@ -112,10 +112,6 @@ class MD {
 		this.body(source.body);
 		// XXX
 		this.content(LIB.app.decorate(source.body));
-		this.tags.removeAll();
-		for (const tag of source.tags) {
-			this.tags.push(new Tag(tag));
-		}
 	}
 
 	copy (source) {
